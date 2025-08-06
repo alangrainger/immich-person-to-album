e57473d6a38fba8c653d1cb923291814ef928123
Alan Grainger <16197738+alangrainger@users.noreply.github.com>
2025-08-06 21:56:05 +0200

    Update logging

--- app/src/index.ts
+++ app/src/index.ts
@@ -5,8 +5,9 @@ import cron from 'node-cron'
 const pta = new PersonToAlbum()
 
 async function main () {
-  for (const user of pta.config.users) {
+  console.log(new Date().toISOString())
 
+  for (const user of pta.config.users) {
     // Init Immich SDK with the specified API key
     init({ baseUrl: pta.config.immichServer + '/api', apiKey: user.apiKey })
 
